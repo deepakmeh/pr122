0154a1bbdb1c3b5371ccf87cef704df2005af32e
deepakmeh <149877729+deepakmeh@users.noreply.github.com>
2024-05-18 15:30:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+createCanvas(350, 350)
+
+function setup() {
+    createCanvas(100, 100);
+  
+    background(200);
+  
+    circle(50, 50, 25);
+  
+    describe('A white circle with black outline in the middle of a gray canvas.');
+  }
+
+  function setup() {
+    createCanvas(100, 100);
+  
+    background(200);
+  
+    ellipse(50, 50, 80, 80);
+  
+    describe('A white circle on a gray canvas.');
+  }
+
+  function setup() {
+    createCanvas(100, 100);
+  
+    background(200);
+  
+    rect(30, 20, 55, 55);
+  
+    describe('A white square with a black outline on a gray canvas.');
+  }
